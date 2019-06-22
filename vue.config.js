f76e81c05257aead9f7c2bc53730fd38f3df4124
marua <meb@mettrr.com>
2019-06-22 11:40:41 +0100

    first commit

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+// vue.config.js
+var path = require('path');
+
+module.exports = {
+  /* eslint-disable indent */
+  /* eslint-disable no-tabs */
+  /* eslint-disable-next-line */
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'scss',
+      patterns: [
+        path.resolve(__dirname, 'src/assets/scss/main.scss'),
+        path.resolve(__dirname, 'src/assets/styles/scss/*.scss'),
+      ],
+    },
+  },
+};
